fb6f646d6c80e1d67019b12290331a583cb2b5c7
Wael Hasan <whma87@gmail.com>
2024-01-13 21:04:53 +0200

    Supporting small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,13 @@ export const metadata: Metadata = {
   title: 'Simple OCR',
   description: 'Extract textual information from photos easily.',
 }
- 
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-evenly p-[1rem]">
+    <main className="
+      flex flex-col lg:flex-row items-center justify-evenly 
+      max-w-[110rem] min-h-screen 
+      p-[1rem] mx-auto">
       <HowToUse />
       <Photo />
     </main>
