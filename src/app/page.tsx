c4d9ea743e8d92980cc25c5d33449cb539254953
Wael Hasan <whma87@gmail.com>
2024-01-13 19:06:18 +0200

    Added metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import type { Metadata } from 'next'
 import { Photo } from "@/components/Photo"
 import { HowToUse } from "@/components/HowToUse"
+
+ 
+export const metadata: Metadata = {
+  title: 'Simple OCR',
+  description: 'Extract textual information from photos easily.',
+}
+ 
 
 export default function Home() {
   return (
