bc209d9948a5840a1b9c0d744e9b7fc6907aa1cd
Wael Hasan <whma87@gmail.com>
2024-01-13 13:12:45 +0200

    Adjust page height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Photo } from "@/components/Photo";
 
 export default function Home() {
   return (
-    <main className="h-screen flex items-center justify-center">
+    <main className="min-h-screen flex items-center justify-center p-[1rem]">
       <Photo />
     </main>
   )
