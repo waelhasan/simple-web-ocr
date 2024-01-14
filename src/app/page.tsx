dc1104b50b740c4ebc4fc0de3ee0b5fe5d6af506
Wael Hasan <whma87@gmail.com>
2024-01-14 21:32:14 +0200

    Refactor metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import type { Metadata } from 'next'
 import { Photo } from "@/components/Photo"
 import { InfoSection } from "@/components/InfoSection"
-
-export const metadata: Metadata = {
-  title: 'Simple OCR',
-  description: 'Extract textual information from photos easily.',
-}
 
 export default function Home() {
   return (
